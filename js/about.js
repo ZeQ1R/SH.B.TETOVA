7b3b1ac23ef8cd39994ff19e475923d11fa64650
Zeq1R <98233629+ZeQ1R@users.noreply.github.com>
2024-09-11 23:51:03 +0200

    Top For all pages

--- js/about.js
+++ js/about.js
@@ -55,3 +55,9 @@ function openModal() {
       header.classList.add('transparent');
     }
   });
+
+  // When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
--- js/peaks.js
+++ js/peaks.js
@@ -53,3 +53,9 @@ function toggleMenu(){
   const navbarRight = document.getElementById("navbar-right");
   navbarRight.classList.toggle("active")
 }
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
